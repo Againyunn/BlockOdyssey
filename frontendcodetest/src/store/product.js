127d929216ea9ddd80148382440ecf1e2d12c1a5
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2023-01-31 20:29:02 +0900

    feat: add isActive state in product state store
    - '필터링' 및 '검색' 기능 활성화 제어를 위한 isActive 상태값과 제어 함수 추가

--- frontendcodetest/src/store/product.js
+++ frontendcodetest/src/store/product.js
@@ -2,13 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const searchProductReducer = createSlice({
   name: "searchProduct",
-  initialState: { filter: false, search: false },
+  initialState: { filter: false, search: false, isActive: false },
   reducers: {
     setFilterData: (state, action) => {
       state.filter = action.payload;
     },
     setSearchData: (state, action) => {
       state.search = action.payload;
+    },
+    setIsActive: (state, action) => {
+      state.isActive = action.payload;
     },
     clearAllData: (state) => {
       state.filter = false;
@@ -17,7 +20,7 @@ export const searchProductReducer = createSlice({
   },
 });
 
-export const { setFilterData, setSearchData, clearProductData } =
+export const { setFilterData, setSearchData, setIsActive, clearProductData } =
   searchProductReducer.actions;
 
 export default searchProductReducer.reducer;
