df92d8d753c04775eab454cd9b157fbbf09bf941
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2023-01-31 20:31:08 +0900

    feat: search & filtering component
    - filter 기능 수행을 위한 data 분리
    - 필터 유형과  검색어를 전역상태에 저장
    - 검색 버튼 클릭 시, isActive 활성화

--- frontendcodetest/src/components/search/Search.jsx
+++ frontendcodetest/src/components/search/Search.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
+// store
+import { useDispatch } from "react-redux";
+import { setIsActive, setFilterData, setSearchData } from "store/product";
+
+// data
+import { optionList } from "components/search/searchData/searchData";
+
 function Search(props) {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const handleSetFilterData = (data) => dispatch(setFilterData(data));
+  const handleSetSearchData = (data) => dispatch(setSearchData(data));
+  const handleSetIsActive = (data) => dispatch(setIsActive(data));
+
+  // 필터 선택 제어
+  const handleFilterSelection = (e) => {
+    handleSetFilterData(e.target.value);
+  };
+
+  // 검색어 입력 제어
+  const handleSearchInput = (e) => {
+    handleSetSearchData(e.target.value === "" ? false : e.target.value);
+  };
+
+  return (
+    <div>
+      <select name="filter" id="" onChange={(e) => handleFilterSelection(e)}>
+        {optionList.map((el) => {
+          return (
+            <option value={el.value} selected={!el.isDefault ? false : true}>
+              {el.name}
+            </option>
+          );
+        })}
+      </select>
+      <input type="text" onChange={(e) => handleSearchInput(e)} />
+      <button onClick={() => handleSetIsActive(true)}>검색</button>
+    </div>
+  );
 }
 
 export default Search;
---
+++ frontendcodetest/src/components/search/searchData/searchData.js
@@ -0,0 +1,6 @@
+export const optionList = [
+  { name: "전체", value: "", isDefault: true },
+  { name: "상품명", value: "name", isDefault: false },
+  { name: "브랜드", value: "brand", isDefault: false },
+  { name: "상품내용", value: "description", isDefault: false },
+];
