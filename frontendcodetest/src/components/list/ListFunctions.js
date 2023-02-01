220c5eb71fbb2ad9987470ad64b97c66e0fd9787
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2023-02-01 10:57:59 +0900

    feat: extract data function
    - 필터, 검색어를 포함하는 데이터 추출 후 반환 함수

---
+++ frontendcodetest/src/components/list/ListFunctions.js
@@ -0,0 +1,50 @@
+export const extractData = (filter, search, data) => {
+  let result = [];
+  let convertedSearch = changeToLowerCase(search);
+
+  switch (filter) {
+    case "name":
+      data.map((item) => {
+        if (changeToLowerCase(item.title.toString()).includes(convertedSearch))
+          result.push(item);
+      });
+      return result;
+
+    case "brand":
+      data.map((item) => {
+        if (changeToLowerCase(item.brand.toString()).includes(convertedSearch))
+          result.push(item);
+      });
+      return result;
+
+    case "description":
+      data.map((item) => {
+        if (
+          changeToLowerCase(item.description.toString()).includes(
+            convertedSearch
+          )
+        )
+          result.push(item);
+      });
+      return result;
+
+    default:
+      if (!search) return data;
+      else {
+        data.map((item) => {
+          if (getKeyByValue(item, convertedSearch)) result.push(item);
+        });
+      }
+      return result;
+  }
+};
+
+const changeToLowerCase = (data) => {
+  return data.toLowerCase();
+};
+
+const getKeyByValue = (object, value) => {
+  return Object.keys(object).find(
+    (key) => changeToLowerCase(object[key].toString()) === value
+  );
+};
