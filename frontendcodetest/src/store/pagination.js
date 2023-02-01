dad8a82adc9ae9e5b48adbd1bc6c98935c9aea30
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2023-02-01 23:41:08 +0900

    feat: create pagination store
    - 페이지네이션 시 전역상태로 처리하기 위한 스토어 생성

---
+++ frontendcodetest/src/store/pagination.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const paginationReducer = createSlice({
+  name: "pagination",
+  initialState: {
+    totalPages: 0,
+    selectedPage: 0,
+    showItems: 10,
+    totalItemLength: 0,
+  },
+  reducers: {
+    setTotalPages: (state, action) => {
+      state.totalPages = action.payload;
+    },
+    setSelectedPage: (state, action) => {
+      state.selectedPage = action.payload;
+    },
+    setShowItems: (state, action) => {
+      state.showItems = action.payload;
+    },
+    setTotalItemLength: (state, action) => {
+      state.totalItemLength = action.payload;
+    },
+  },
+});
+
+export const {
+  setTotalPages,
+  setSelectedPage,
+  setShowItems,
+  setTotalItemLength,
+} = paginationReducer.actions;
+
+export default paginationReducer.reducer;
--- frontendcodetest/src/store/store.js
+++ frontendcodetest/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchProductReducer from "store/product";
+import paginationReducer from "store/pagination";
 
 export const store = configureStore({
   reducer: {
     searchProduct: searchProductReducer,
+    pagination: paginationReducer,
   },
 });
