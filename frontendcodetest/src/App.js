3a85f905b503a13faa5d4962f5b9bfe495cc5684
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2023-02-01 12:28:34 +0900

    feat: product list page
    - api 초기 호출 및 데이터 바인딩
    - 필터링 & 검색 기능 연결 후 데이터 바인딩
    - 데이터에 대한 페이지 연산
    - list와 pagination 연결

--- frontendcodetest/src/App.js
+++ frontendcodetest/src/App.js
@@ -1,15 +1,24 @@
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 // pages
 import ProductList from "pages/ProductList";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: Infinity,
+      staleTime: Infinity,
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
         <ProductList />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </div>
   );
--- frontendcodetest/src/pages/ProductList.jsx
+++ frontendcodetest/src/pages/ProductList.jsx
@@ -1,16 +1,111 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// react-qeury
+import { useQuery } from "react-query";
+
+// store
+import { useDispatch, useSelector } from "react-redux";
+import { setIsActive } from "store/product";
+
+// function
+import { extractData } from "components/list/ListFunctions";
 
 // components
 import List from "components/list/List";
 import Pagination from "components/pagination/Pagination";
 import Search from "components/search/Search";
 
+// API
+import { getProductData } from "services/Products";
+
 function Productlist(props) {
+  /**STATE */
+  //
+  const [isUpdated, setIsUpdated] = useState(0);
+
+  // product 제어
+  const [rawProducts, setRawProducts] = useState([]); // API raw data
+  const [products, setProducts] = useState([]); // filtered searched data
+  const [currentProducts, setCurrentProducts] = useState([]); // paginated data
+
+  // pagination 제어
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
+  const [showProductsOnce, setShowProductOnce] = useState(10); // n개씩 보기
+  const [totalPagesNumber, setTotalPagesNumber] = useState();
+
+  // store 제어
+  const searchData = useSelector((state) => state.searchProduct);
+  const dispatch = useDispatch();
+  const handleIsActive = (data) => dispatch(setIsActive(data));
+
+  /** API 데이터 받아오기 */
+  const { isLoading, error, refetch, isSuccess } = useQuery("products", () =>
+    getProductData()
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+
+        setRawProducts(data.products);
+      })
+  );
+
+  useEffect(() => {
+    if (!products || !searchData.isActive) {
+      return;
+    }
+
+    if (searchData.isActive) {
+      refetch();
+      setIsUpdated(1);
+    }
+  }, [searchData.isActive]);
+
+  useEffect(() => {
+    if (isUpdated === 2) {
+      if (!searchData.search)
+        // 검색어 입력 여부 식별
+        setProducts(rawProducts);
+      else {
+        /** 필터링 & 검색 데이터 추출 */
+        let extractedData = extractData(
+          searchData.filter,
+          searchData.search,
+          rawProducts
+        );
+        setProducts(extractedData);
+      }
+      handleIsActive(false);
+
+      setIsUpdated(0);
+    }
+  }, [isUpdated]);
+
+  useEffect(() => {
+    setIsUpdated(2);
+  }, [rawProducts]);
+
+  useEffect(() => {
+    calculatePagination(showProductsOnce, products);
+  }, [products, currentPage]);
+
+  const calculatePagination = (showProductsOnce, products) => {
+    let lastIndex = currentPage * showProductsOnce;
+    let firstIndex = lastIndex - showProductsOnce;
+
+    setCurrentProducts(products.slice(firstIndex, lastIndex));
+    setTotalPagesNumber(Math.ceil(products.length / showProductsOnce));
+  };
+
   return (
     <div>
       <Search />
-      <List />
-      <Pagination />
+      <List
+        currentProductData={currentProducts}
+        productDataLength={products.length}
+      />
+      <Pagination pages={totalPagesNumber} setCurrentPage={setCurrentPage} />
     </div>
   );
 }
