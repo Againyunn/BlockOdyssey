f4684b588b10690edc4e1b87b2e9bb6d43609e9b
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2023-02-01 11:05:52 +0900

    feat: pagination component
    - 각 버튼 선택에 따른 버튼 상태 변경
    - 상위 컴포넌트에서 props 전달받아 처리하도록 구현

--- frontendcodetest/src/components/pagination/Pagination.jsx
+++ frontendcodetest/src/components/pagination/Pagination.jsx
@@ -1,14 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-// components
-import PaginationFilter from "components/pagination/PaginationFilter";
+function Pagination({ pages, setCurrentPage }) {
+  // state
+  const [selectedButton, setSelectedButton] = useState(1); // 현재 선택된 버튼
+  const [currentButtonArray, setCurrentButtonArray] = useState([]); // 현재 표시될 페이지 번호들
 
-function Pagination(props) {
+  const pageNumberArray = [];
+  for (let i = 1; i <= pages; i++) {
+    pageNumberArray.push(i);
+  }
+
+  useEffect(() => {
+    let tmpPageNumberArray = [...currentButtonArray];
+
+    let leftDots = "...";
+    let rightDots = "...";
+
+    // 페이지 번호 배열 내 ... 의 유무 식별
+    if (pageNumberArray.length < 7) {
+      tmpPageNumberArray = pageNumberArray;
+    } else {
+      // [1 2 3 4 5 ... last]
+      if (1 <= selectedButton && selectedButton <= 4) {
+        tmpPageNumberArray = [1, 2, 3, 4, 5, rightDots, pageNumberArray.length];
+      }
+      // [1 ... 4 5 6 ... last]  ~  [1 ... last-4 last-3 last-2 ... last]
+      else if (
+        selectedButton > 4 &&
+        selectedButton < pageNumberArray.length - 2
+      ) {
+        const sliced = pageNumberArray.slice(
+          selectedButton - 2,
+          selectedButton + 1
+        );
+        tmpPageNumberArray = [
+          1,
+          leftDots,
+          ...sliced,
+          rightDots,
+          pageNumberArray.length,
+        ];
+      }
+      // [1 ... last-4 last-3 last-2 last-1 last]
+      else if (selectedButton > pageNumberArray.length - 3) {
+        const sliced = pageNumberArray.slice(pageNumberArray.length - 5);
+        tmpPageNumberArray = [1, leftDots, ...sliced];
+      }
+
+      // 각 위치의 ... 이 선택되었을 때
+      else if (selectedButton === rightDots) {
+        setSelectedButton(currentButtonArray[3] + 2);
+      } else if (selectedButton === leftDots) {
+        setSelectedButton(currentButtonArray[3] - 2);
+      }
+    }
+
+    setCurrentButtonArray(tmpPageNumberArray);
+    setCurrentPage(selectedButton);
+  }, [selectedButton, pages]);
+
   return (
     <div>
-      <PaginationFilter />
+      <button
+        onClick={() =>
+          setSelectedButton((prev) => (prev <= 1 ? prev : prev - 1))
+        }
+      >
+        전
+      </button>
+
+      {currentButtonArray.map((el) => {
+        return <button onClick={() => setSelectedButton(el)}>{el}</button>;
+      })}
+
+      <button
+        onClick={() =>
+          setSelectedButton((prev) =>
+            prev >= pageNumberArray.length ? prev : prev + 1
+          )
+        }
+      >
+        후
+      </button>
     </div>
   );
 }
-
 export default Pagination;
