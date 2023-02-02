4bf7371c9543e612df4f602d5bfda8ef2a5908c3
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2023-02-02 11:14:47 +0900

    chore: fix table component's structure
    - table 구조 수정(thead, tbody 간의 분리 누락 수정)

--- frontendcodetest/src/components/list/List.jsx
+++ frontendcodetest/src/components/list/List.jsx
@@ -25,64 +25,70 @@ function List(props) {
       </div>
 
       <table className="list-table-wrap ">
-        <th className="list-table-header width-middle horizon-line">
-          <span className="bord-text">상품번호</span>
-        </th>
-        <th className="list-table-header width-extra-large horizon-line">
-          <span className="bord-text">상품명</span>
-        </th>
-        <th className="list-table-header width-large horizon-line">
-          <span className="bord-text">브랜드</span>
-        </th>
-        <th className="list-table-header width-extra-large horizon-line">
-          <span className="bord-text">상품내용</span>
-        </th>
-        <th className="list-table-header width-small horizon-line">
-          <span className="bord-text">가격</span>
-        </th>
-        <th className="list-table-header width-small horizon-line">
-          <span className="bord-text">평점</span>
-        </th>
-        <th className="list-table-header width-small horizon-line">
-          <span className="bord-text">재고</span>
-        </th>
-        {!!products &&
-          products.map((el, idx) => {
-            let thisDescription = !el.description
-              ? ""
-              : getTestSnippet(el.description, 40);
-            let thisPrice = !el.price ? "" : getPriceData(el.price);
-            let thisRating = !el.rating ? "" : setComma(el.rating, 2);
-            let thisStock = !el.stock ? "" : setComma(el.stock, 3);
+        <thead>
+          <tr>
+            <td className="list-table-header width-middle horizon-line">
+              <span className="bord-text">상품번호</span>
+            </td>
+            <td className="list-table-header width-extra-large horizon-line">
+              <span className="bord-text">상품명</span>
+            </td>
+            <td className="list-table-header width-large horizon-line">
+              <span className="bord-text">브랜드</span>
+            </td>
+            <td className="list-table-header width-extra-large horizon-line">
+              <span className="bord-text">상품내용</span>
+            </td>
+            <td className="list-table-header width-small horizon-line">
+              <span className="bord-text">가격</span>
+            </td>
+            <td className="list-table-header width-small horizon-line">
+              <span className="bord-text">평점</span>
+            </td>
+            <td className="list-table-header width-small horizon-line">
+              <span className="bord-text">재고</span>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {!!products &&
+            products.map((el, idx) => {
+              let thisDescription = !el.description
+                ? ""
+                : getTestSnippet(el.description, 40);
+              let thisPrice = !el.price ? "" : getPriceData(el.price);
+              let thisRating = !el.rating ? "" : setComma(el.rating, 2);
+              let thisStock = !el.stock ? "" : setComma(el.stock, 3);
 
-            return (
-              <tr key={`list-table-content-${idx}`}>
-                <td className="list-table-body horizon-line">
-                  <span className="default-text">{el.id}</span>
-                </td>
-                <td className="list-table-body horizon-line">
-                  <span className="default-text">{el.title}</span>
-                </td>
-                <td className="list-table-body horizon-line">
-                  <span className="default-text">{el.brand}</span>
-                </td>
+              return (
+                <tr key={`list-table-content-${idx}`}>
+                  <td className="list-table-body horizon-line">
+                    <span className="default-text">{el.id}</span>
+                  </td>
+                  <td className="list-table-body horizon-line">
+                    <span className="default-text">{el.title}</span>
+                  </td>
+                  <td className="list-table-body horizon-line">
+                    <span className="default-text">{el.brand}</span>
+                  </td>
 
-                <td className="list-table-body horizon-line">
-                  <span className="default-text">{thisDescription}</span>
-                </td>
+                  <td className="list-table-body horizon-line">
+                    <span className="default-text">{thisDescription}</span>
+                  </td>
 
-                <td className="list-table-body horizon-line">
-                  <span className="default-text">{thisPrice}</span>
-                </td>
-                <td className="list-table-body horizon-line">
-                  <span className="default-text">{thisRating}</span>
-                </td>
-                <td className="list-table-body horizon-line">
-                  <span className="default-text">{thisStock}</span>
-                </td>
-              </tr>
-            );
-          })}
+                  <td className="list-table-body horizon-line">
+                    <span className="default-text">{thisPrice}</span>
+                  </td>
+                  <td className="list-table-body horizon-line">
+                    <span className="default-text">{thisRating}</span>
+                  </td>
+                  <td className="list-table-body horizon-line">
+                    <span className="default-text">{thisStock}</span>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
       </table>
     </React.Fragment>
   );
